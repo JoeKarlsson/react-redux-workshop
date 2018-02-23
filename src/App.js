5b3a3ae4daec6c5897ceeeef6b604c7204d79c27
Joe Karlsson <josephcarlson1@gmail.com>
2018-02-23 11:30:14 -0600

    updated zero state.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Reddit</h1>
         </header>
+				<h3>Edit src/App.js to get started!</h3>
       </div>
     );
   }
